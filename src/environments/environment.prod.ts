06e6d0f1fb98ab01356cacfb2a91d1b978f6d7b1
chetryJyoti <jyotichetry087@gmail.com>
2023-05-24 16:16:39 +0530

    added env required template for others

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  ENDPOINT: 'https://cloud.appwrite.io/v1',
+  PROJECT_ID: '',
+  BUCKET_ID: '',
+
+  //appwrite db cred
+  APPWRITE_DB_ID: '',
+  APPWRITE_COLLECTION_ID: '',
+  APPWRITE_API_KEY:
+    '',
+  //for home page
+    APPWRITE_DOC_ID:'',
+
+  //thingspeak api keys
+  THINGS_LINK: 'https://api.thingspeak.com/channels',
+  THINGS_UPDATE_URI: 'https://api.thingspeak.com/update.json',
+  THINGS_CHANNEL_ID: '',
+  THINGS_READ_API_KEY: '',
+  THINGS_WRITE_API_KEY: '',
 };
