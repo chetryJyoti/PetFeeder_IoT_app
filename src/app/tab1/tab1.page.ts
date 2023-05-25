27ae7d660a90ca41306017988ab12ccb6b6cc5ba
chetryJyoti <jyotichetry087@gmail.com>
2023-05-25 14:38:03 +0530

    added feeding funcationality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,35 +14,14 @@ export class Tab1Page implements OnInit {
   disableButton: boolean = false;
   currentFeedCount = 0;
   totalFeedCount = 0;
-  currentMarkerPosition: string = '5%';
-  initialMarkerPosition = 0;
-  markerPositions: string[] = [
-    '5%',
-    '10%',
-    '15%',
-    '20%',
-    '25%',
-    '30%',
-    '35%',
-    '40%',
-    '45%',
-    '50%',
-    '55%',
-    '60%',
-    '65%',
-    '70%',
-    '75%',
-    '80%',
-    '85%',
-    '90%',
-    '95%',
-  ];
+  lastFeedDateTime: string = '';
 
   //means when the currentFeedCount reaches 19 the food is finished(this will be a hard coded value)
-  emptyFoodCount = 19;
+  FOOD_EMPTY_ON = 19;
 
+  foodLevelValue = 0;
   foodEmptyMessage: string = '';
-  emptyFoodColor: string = 'var(--ion-color-success)';
+
   options: any;
   selectedImage: string = '../../assets/images/bruno.jpeg';
   constructor(
@@ -51,47 +30,25 @@ export class Tab1Page implements OnInit {
     private alertController: AlertController,
     private imagePicker: ImagePicker
   ) {
-
+    //fun to change pick images
   }
   imagePkr() {
     this.options = {
       width: 60,
       height: 60,
       quality: 30,
-      outputType: 1
+      outputType: 1,
     };
 
-    this.imagePicker.getPictures(this.options).then((results) => {
-      if (results.length > 0) {
-        this.selectedImage = results[0];
-        console.log('Image URI: ' + this.selectedImage);
-      }
-    }, (err) => { });
-  }
-
-  updateMarkerPosition() {
-    this.emptyFoodCount -= 1;
-
-    if (this.emptyFoodCount <= 0) {
-      this.currentMarkerPosition =
-        this.markerPositions[this.markerPositions.length - 1];
-      this.foodEmptyMessage = 'Food is empty';
-      this.emptyFoodColor = 'var(--ion-color-danger)';
-    } else {
-      this.initialMarkerPosition += 1;
-
-      if (this.initialMarkerPosition >= this.markerPositions.length) {
-        this.initialMarkerPosition = 0;
-      }
-
-      this.currentMarkerPosition =
-        this.markerPositions[this.initialMarkerPosition];
-      this.foodEmptyMessage = '';
-    }
-
-    console.log(this.emptyFoodCount);
-    console.log(this.currentMarkerPosition);
-    console.log(this.foodEmptyMessage);
+    this.imagePicker.getPictures(this.options).then(
+      (results) => {
+        if (results.length > 0) {
+          this.selectedImage = results[0];
+          console.log('Image URI: ' + this.selectedImage);
+        }
+      },
+      (err) => {}
+    );
   }
 
   async presentAlertForResetFeedCounts() {
@@ -114,40 +71,50 @@ export class Tab1Page implements OnInit {
     await alert.present();
   }
 
+  getCurrentDateTime() {
+    const now = new Date();
+    const date = now.toLocaleDateString('en-US', {
+      year: '2-digit',
+      month: 'numeric',
+      day: 'numeric',
+    });
+    const time = now.toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    });
+    this.lastFeedDateTime = `${date} at ${time}`;
+  }
+
+  foodLevelStatus() {
+    if (this.foodLevelValue >= 5 && this.foodLevelValue <= 10) {
+      this.foodEmptyMessage = 'Okay';
+      console.log(this.foodEmptyMessage);
+      console.log(this.foodLevelValue);
+    } else if (this.foodLevelValue > 10 && this.foodLevelValue <= 15) {
+      this.foodEmptyMessage = 'Mid way ';
+      console.log(this.foodLevelValue);
+      console.log(this.foodEmptyMessage);
+    } else if (this.foodLevelValue > 15 && this.foodLevelValue <= 19) {
+      this.foodEmptyMessage = 'almost finished critical';
+      console.log(this.foodLevelValue);
+      console.log(this.foodEmptyMessage);
+    } else if (this.foodLevelValue == 20) {
+      this.foodEmptyMessage = 'empty';
+      console.log(this.foodEmptyMessage);
+      console.log(this.foodLevelValue);
+    }
+  }
+
   ngOnInit() {
     //for testing purposes iam stoping api calls
-    // this.getFeedData();
-    // this.getTotalFeedCounts();
-  }
-  async getFeedData() {
-    await this.appwriteService
-      .listDocuments(
-        environment.APPWRITE_DB_ID,
-        environment.APPWRITE_COLLECTION_ID
-      )
-      .then((response) => {
-        const documents = response.documents;
-        console.log('documents:', documents);
-        if (documents.length > 0) {
-          const firstDocument = documents[0];
-          this.currentFeedCount = firstDocument.FeedTimes;
-          console.log(
-            'Feed count retrieved successfully:',
-            this.currentFeedCount
-          );
-        } else {
-          console.log('No documents found in the collection');
-        }
-      })
-      .catch((error) => {
-        console.log('Error retrieving feed count:', error);
-      });
+    this.getFeedDetails();
   }
 
   async resetFeedCount() {
     this.currentFeedCount = 0;
     const data = {
-      FeedTimes: 0, // Pass the integer value directly
+      FeedTimes: 0,
     };
     await this.appwriteService
       .updateDocument(
@@ -164,13 +131,17 @@ export class Tab1Page implements OnInit {
       });
   }
 
-  async updateFeedCount() {
+  async updateFeedDetails() {
+    this.foodLevelValue = this.FOOD_EMPTY_ON - this.currentFeedCount;
+    this.foodLevelStatus();
     this.currentFeedCount += 1;
     this.totalFeedCount += 1;
-    this.updateMarkerPosition();
+    this.getCurrentDateTime();
     const data = {
       FeedTimes: this.currentFeedCount, // Pass the integer value directly
       totalFeedCount: this.totalFeedCount,
+      LastFeedDateTime: this.lastFeedDateTime,
+      foodLevel:this.foodLevelValue
     };
     await this.appwriteService
       .updateDocument(
@@ -187,14 +158,16 @@ export class Tab1Page implements OnInit {
       });
   }
 
+  //on off raspberry pi connected servo motor
   async toggleServo() {
-    this.updateFeedCount();
+    this.updateFeedDetails();
     this.servoState = this.servoState === 'on' ? 'off' : 'on';
     const data = {
       api_key: environment.THINGS_WRITE_API_KEY,
       field1: this.servoState === 'on' ? '1' : '0',
     };
 
+    //talking to thingspeak
     await this.http.post(environment.THINGS_UPDATE_URI, data).subscribe(() => {
       console.log(`Servo toggled ${this.servoState}`);
       this.disableButton = true;
@@ -214,8 +187,7 @@ export class Tab1Page implements OnInit {
     });
   }
 
-  ///total feed counts
-  async getTotalFeedCounts() {
+  async getFeedDetails() {
     await this.appwriteService
       .listDocuments(
         environment.APPWRITE_DB_ID,
@@ -227,6 +199,9 @@ export class Tab1Page implements OnInit {
         if (documents.length > 0) {
           const firstDocument = documents[0];
           this.totalFeedCount = firstDocument.totalFeedCount;
+          this.currentFeedCount = firstDocument.FeedTimes;
+          this.lastFeedDateTime = firstDocument.LastFeedDateTime;
+          this.foodLevelValue=firstDocument.foodLevel
           console.log(
             'Total Feed count retrieved successfully:',
             this.currentFeedCount
