e1247a82fe6718049eac4f6d491b2698b28d9e6a
chetryJyoti <jyotichetry087@gmail.com>
2023-05-25 14:39:04 +0530

    changed appname

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Pet_feeder_app',
+  appId: 'io.ionic.Chetry',
+  appName: 'PetFeeder',
   webDir: 'www',
   server: {
     androidScheme: 'https'
