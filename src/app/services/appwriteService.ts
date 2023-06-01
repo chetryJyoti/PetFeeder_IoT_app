b151f822104f6a460c208cc206fabed6ab0a813c
chetryJyoti <jyotichetry087@gmail.com>
2023-06-01 23:09:38 +0530

    created appwrite auth services

--- src/app/services/appwriteService.ts
+++ src/app/services/appwriteService.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Client, Storage,Databases } from 'appwrite';
+import { Client, Storage, Databases, Account, ID } from 'appwrite';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppwriteService {
+  private account: Account;
   private client: Client;
   private storage: Storage;
   private databases: Databases;
@@ -14,17 +15,44 @@ export class AppwriteService {
     this.client = new Client();
     this.storage = new Storage(this.client);
     this.databases = new Databases(this.client);
-
+    this.account = new Account(this.client);
     this.client
       .setEndpoint(environment.ENDPOINT) // Your API Endpoint
-      .setProject(environment.PROJECT_ID) // Your project ID
+      .setProject(environment.PROJECT_ID); // Your project ID
+  }
+
+  //for auth
+  signup(email: string, password: string): Promise<any> {
+    const promise = this.account.create(ID.unique(), email, password);
+    return promise;
+  }
+
+  // Login function
+  login(email: string, password: string): Promise<any> {
+    const promise = this.account.createEmailSession(email, password);
+    console.log('login:', promise);
+
+    return promise;
+  }
+  getCurrentLogInUser(){
+    return this.account.get()
   }
 
   getAllImages(bucketId: string): Promise<any> {
     return this.storage.listFiles(bucketId);
   }
-  updateDocument(databaseId: string, collectionId: string, documentId: string, updatedData:any): Promise<any> {
-    return this.databases.updateDocument(databaseId, collectionId, documentId, updatedData);
+  updateDocument(
+    databaseId: string,
+    collectionId: string,
+    documentId: string,
+    updatedData: any
+  ): Promise<any> {
+    return this.databases.updateDocument(
+      databaseId,
+      collectionId,
+      documentId,
+      updatedData
+    );
   }
 
   listDocuments(databaseId: string, collectionId: string): Promise<any> {
@@ -33,5 +61,4 @@ export class AppwriteService {
   deleteImage(bucketId: string, fileId: string): Promise<any> {
     return this.storage.deleteFile(bucketId, fileId);
   }
-
 }
